6904ed744a26f8615f0841d76ac4d91a392e5c5b
406021-GAUFFIN <406021@tecnicatura.frc.utn.edu.ar>
2025-04-25 11:22:11 -0300

    devops changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,18 +16,31 @@ import { EmailModule } from './features/email/email.module';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: configService.get<string>('NODE_ENV') === 'development',
-        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-        migrationsTableName: 'migrations',
-      }),
+      useFactory: (configService: ConfigService) => {
+        if (configService.get('DATABASE_URL')) {
+          return {
+            type: 'postgres',
+            url: configService.get('DATABASE_URL'),
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+            migrationsTableName: 'migrations',
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          };
+        }
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+          migrationsTableName: 'migrations',
+        };
+      },
     }),
 
     UsersModule,
--- typeorm.config.ts
+++ typeorm.config.ts
@@ -3,14 +3,27 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export default new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_DATABASE || 'mydb',
-  entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
-  migrations: [__dirname + '/src/migrations/**/*{.ts,.js}'],
-  migrationsTableName: 'migrations',
-});
+const config = process.env.DATABASE_URL
+  ? {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
+      migrations: [__dirname + '/src/migrations/**/*{.ts,.js}'],
+      migrationsTableName: 'migrations',
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_DATABASE || 'mydb',
+      entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
+      migrations: [__dirname + '/src/migrations/**/*{.ts,.js}'],
+      migrationsTableName: 'migrations',
+    };
+
+export default new DataSource(config as any);
